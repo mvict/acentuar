79fb35ba6345aa76d0f1e96ed241caddc198ca6b
Canelaguil <canelaguil@gmail.com>
2021-03-13 17:22:02 +0100

    dutch spell check

--- localisation.py
+++ localisation.py
@@ -1,116 +1,116 @@
 ADVICE_YES = {
     "en": "Yes, you should write an accent in syllable number {}.",
     "es": "Sí, escriba un acento en la sílaba {}.",
-    "nl": "Ja, schrijf maar een accent op lettergreep {}."
-    }
+    "nl": "Ja, lettergreep {} heeft een accent."
+}
 
 ADVICE_NO = {
     "en": "No accent needed, leave it like that:{}.",
     "es": "No hace falta ningún acento. Déjela así: {}.",
-    "nl": "U hoeft geen accent te schrijven. Laat maar zo: {}."
-    }
+    "nl": "Hier is geen accent nodig, {} is goed."
+}
 
 AGUDAS_VOWEL_N_S = {
     "en": "A 'aguda' word takes the accent if ends in vowel, 'n' or 's.'",
     "es": "Las palabras agudas se acentúan si acaban en vocal, en 'n' o en 's'.",
-    "nl": "Aguda-woorden krijgen accent als ze met een klinker, 'n' or 's' eindigen."
-    }
+    "nl": "Aguda-woorden krijgen een accent als ze met een klinker, 'n' or 's' eindigen."
+}
 
 AGUDAS_SOMETHING_ELSE = {
     "en": "A 'aguda' word takes the accent if ends in vowel, 'n' or 's' and it's not the case.",
     "es": "Las palabras agudas se acentúan si acaban en vocal, en 'n' o en 's' y este no es el caso.",
-    "nl": "Aguda-woorden krijgen accent slechts als ze met een klinker, 'n' or 's' eindigen."
-    }
+    "nl": "Aguda-woorden krijgen slechts een accent als ze met een klinker, 'n' or 's' eindigen."
+}
 
 ALL_ESDRUJULAS = {
     "en": "All 'esdrújulas' words are written with accent.",
     "es": "Todas las esdrújulas se acentúan.",
-    "nl": "Alle 'esdrújulas' krijgen een accent."
-    }
+    "nl": "'Esdrújulas' hebben altijd een accent."
+}
 
 DIACRITIC_ALREADY_USE = {
     "en": "You already wrote an accent, please give me a word without diacritics.",
-    "es": "Esta palabra ya está acentuada, inténtelo de nuevo",
-    "nl": "Dit woord heeft al een accent, probeer het nog een keer"
-    }
+    "es": "Esta palabra ya está acentuada, inténtelo de nuevo.",
+    "nl": "Dit woord heeft al een accent, probeer het nog een keer."
+}
 
 EMPHASIS_EXPLANATION = {
     "en": "You had to point out where you hear the emphasis in the word.",
     "es": "Tenía que indicar dónde escucha el acento al pronunciar la palabra.",
-    "nl": "U had moeten aangeven waar u de klemtoon in dit woord hoort."
-   }
+    "nl": "Je had moeten aangeven waar je de klemtoon in dit woord hoort."
+}
 
 FEEDBACK_OK = {
     "en": "Great!",
     "es": "¡Muy bien!",
-    "nl": "Prima!"
-    }
+    "nl": "Goed zo!!"
+}
 
 FEEDBACK_WRONG = {
     "en": "No, no, no, it is: {}.",
     "es": "No, no, no, es: {}.",
-    "nl": "No, no, no, het is: {}."
-  }
+    "nl": "Nee, nee, nee! Het is: {}."
+}
 
 GOOD_LUCK = {
     "en": "Good luck with next word!",
     "es": "¡A ver si hay más suerte con la siguiente!",
-    "nl": "Succes met het volgende word!"
-             }
+    "nl": "Hopelijk gaat het volgende woord beter!"
+}
 
 HEARD_EMPHASIS = {
     "en": "In which syllable do you hear the accent? (1, 2, 3 counting from behind)."
-            "\nSay 0 if you don't know >>> ",
+    "\nSay 0 if you don't know >>> ",
     "es": "¿En qué sílaba oye el acento? (1, 2, 3 empezando por el final)"
-            "\nDiga 0 si no lo sabe >>> ",
-    "nl": "Op welk lettergrep hoort u de klemton? (1, 2, 3 van achteren). "
-            "\nZeg 0 als u het niet weet >>> "
-    }
+    "\nDiga 0 si no lo sabe >>> ",
+    "nl": "Op welk lettergreep hoor je de klemtoon? (1, 2, 3, vanaf achteren). "
+    "\nZeg 0 als je het niet weet >>> "
+}
 
 HELP_BY_ACCENT_TEXT = {
     "es": "Ayuda ",
     "en": "Help should be given and taken",
-    "nl": "Hulp moet hier aangeboden worden"
-  }
+    "nl": "Help"
+}
 
 HOW_MANY_TIMES = {
     "en": "How many times would you like to play? (1-10)   >>>  ",
     "es": "¿Cuántas veces quieres jugar? (1-10)   >>>  ",
     "nl": "Hoeveel keer wilt u spelen? (1-10)   >>> "
-    }
+}
 
 LLANAS_SOMETHING_ELSE = {
     "en": "A 'llana' word takes the accent if it doesn't end in vowel, 'n' or 's'",
     "es": "Las palabras llanas se acentúan si no acaban en vocal, en n o en s",
-    "nl": "Llana-woorden krijgen accent als ze niet met een klinker, 'n' or 's' eindigen"
-    }
+    "nl": "Llana-woorden krijgen een accent als ze niet met een klinker, 'n' or 's' eindigen"
+}
 
 LLANAS_VOWEL_N_S = {
     "en": "A 'llana' word takes the accent if it doesn't end in vowel, 'n' or 's'",
     "es": "Las palabras llanas no se acentúan si acaban en vocal, en n o en s",
     "nl": "Llana-woorden krijgen geen accent als ze met een klinker, 'n' or 's' eindigen"
-    }
+}
 
 LIKE_THAT = {
     "en": "Like that: {}",
     "es": "Así: {}",
     "nl": "Zo: {}"
-    }
+}
 
 SYLLABICATION_ERROR = {
-    "en": "Syllabication error occurred while splitsing the word",
+    "en": "Syllabication error occurred while splitting the word",
     "es": "Error al silabizar la palabra",
-    "nl": "Er is iets mis met de splitsing in lettergreppen gegaan"
-  }
+    "nl": "Er is iets misgegaan met het opsplitsen van het woord."
+}
 
 WHICH_SYLLABLE = {
     "en": "¿En qué sílaba se acentúa {}? ",
     "es": "In which syllable does the emphasis fall in {}? ",
     "nl": "Op welk lettergreep valt de klemtoon in {}? "
-    }
+}
 
 WHICH_WORD = {
     "en": "Give me a word you don't know how to write >> ",
     "es": "¿Qué palabra no sabe acentuar? >> ",
-    "nl": "Welk word wilt u weten? >> "
-    }
+    "nl": "Met welk woord heb je hulp nodig? >> "
+}
